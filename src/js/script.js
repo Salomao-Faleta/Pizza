bd0fdc2a10401d0982793988dbe7f2642c061ece
Salomão Faleta <faletas41@gmail.com>
2023-09-04 23:05:20 -0300

    Adicionado os itens na tela

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+const c = (el) =>{ return document.querySelector(el)}
+const cs = (el) =>{ return document.querySelectorAll(el)}
+
+pizzaJson.map(function(item, index){
+    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
+    //preencher as informações em pizzaItem
+
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+
+    c('.pizza-area').append(pizzaItem)
+
+})
