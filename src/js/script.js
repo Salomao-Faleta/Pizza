d5111090038f07526eccf48a3173da21f0cbf7b7
Salomão Faleta <faletas41@gmail.com>
2023-09-05 21:10:48 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+let modalQl = 1;
+
 const c = (el) =>{ return document.querySelector(el)}
 const cs = (el) =>{ return document.querySelectorAll(el)}
 
@@ -12,15 +14,22 @@ pizzaJson.map(function(item, index){
     pizzaItem.querySelector('a').addEventListener('click', (e)=>{
         e.preventDefault()
 
-        let key = e.target.closest('.pizza-item').getAttribute('data-key')
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQl = 1;
 
         c('.pizzaBig img').src = pizzaJson[key].img
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         c('.pizzaInfo--actualPrice').innerHTML = `R$${pizzaJson[key].price.toFixed(2)}`
+        c('.pizzaInfo--size.selected').classList.remove('selected');
         cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+            if(sizeIndex == 2){
+                size.classList.add('selected');
+            }
             size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
         })
+
+        c('.pizzaInfo--qt').innerHTML = modalQl;
 
 
         //abrindo o modal
